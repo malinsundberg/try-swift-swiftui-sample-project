c57b9392d4eb8a5f43e9063f4f31689fb38cfe1c
Malin Sundberg <mail@malinsundberg.com>
2021-03-29 20:59:04 -0700

    Added files to be used as starting points

---
+++ Shared/Composed Views/Chart View/ChartView.swift
@@ -0,0 +1,20 @@
+//
+//  ChartView.swift
+//  Coffee Counter (iOS)
+//
+//  Created by Malin Sundberg on 2021-03-28.
+//
+
+import SwiftUI
+
+struct ChartView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct ChartView_Previews: PreviewProvider {
+    static var previews: some View {
+        ChartView()
+    }
+}
---
+++ Shared/Composed Views/Chart View/ChartViewWithAxes.swift
@@ -0,0 +1,20 @@
+//
+//  ChartViewWithAxes.swift
+//  Coffee Counter (iOS)
+//
+//  Created by Malin Sundberg on 2021-03-28.
+//
+
+import SwiftUI
+
+struct ChartViewWithAxes: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct ChartViewWithAxes_Previews: PreviewProvider {
+    static var previews: some View {
+        ChartViewWithAxes()
+    }
+}
